2571f37506b275542c7ba469bba545b439c40695
conrad96 <conrad.mugisha@gmail.com>
2020-01-05 12:38:14 +0300

    testing bootstrap

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path')
+const window = require('./window');
 
 function mainWindow(){
-
+    const firstWindow = new window({
+        file: 'index.html'
+    })        
 }
 
 app.on('ready', mainWindow);
